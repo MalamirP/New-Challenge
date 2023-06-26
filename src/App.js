83ee699b8c347d714915cd46dec4988416ecfda0
Malamir P <mimoe@abv.bg>
2023-06-26 15:40:55 +0300

    git request upload with bug

--- src/App.js
+++ src/App.js
@@ -6,9 +6,38 @@ import ViewCategoryPage from './pages/ViewCategory/ViewCategoryPage';
 import Header from './components/Header/Header';
 import CategoryIT from './pages/Category-IT/Category-IT';
 import LoginPage from './pages/LoginPage/LoginPage';
+import { useEffect } from 'react';
 
 function App() {
 
+  // useEffect(() => {
+  //   fetch('188.166.21.103:4000/category?language=en', {
+  //     headers: {
+  //       'Accept': 'application/json',
+  //     }
+  //   })
+  //     .then(response => response.text())
+  //     .then(text => console.log(text))
+  // })
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    console.log(token);
+    fetch('188.166.21.103:4000/category?language=en', {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(`Error:, ${error}`);
+      });
+  }, []);
 
   return (
     <div>
